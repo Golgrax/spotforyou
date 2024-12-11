625c187e7b3ae296ded798c57c4b6e57d9586643
Golgrax <pepe.kotelurow@gmail.com>
2024-12-11 20:25:02 +0800

    first commit

---
+++ werepro.js
@@ -0,0 +1,8 @@
+$('a.brand,a.work-link,a.cta-link,a.credit-link,a.nav-link,a.txt-link,a.nav-brand,a.project-link,.work-link,.mob-button').click(function (e) {
+    e.preventDefault();        
+    var goTo = this.getAttribute("href"); 
+
+    setTimeout(function(){
+         window.location = goTo;
+    },1000);       
+}); 
